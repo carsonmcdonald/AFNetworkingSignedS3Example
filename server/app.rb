43a32bc131e760b13ebe20081fe3c38e2cde7968
Carson McDonald <carson@ioncannon.net>
2013-06-21 10:52:00 -0400

    Add support for returning a json response

--- server/app.rb
+++ server/app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'multi_json'
+require "sinatra/json"
 require 'base64'
 require 'openssl'
 require 'cgi'
@@ -10,7 +12,7 @@ S3_BUCKET='/uploadtestbucket'
 EXPIRE_TIME=(60 * 5) # 5 minutes
 S3_URL='http://s3.amazonaws.com'
 
-get '/signput' do
+get '/signput.json' do
   object_name = "/#{params['name']}"
 
   mime_type = params['type']
@@ -20,5 +22,5 @@ get '/signput' do
   string_to_sign = "PUT\n\n#{mime_type}\n#{expires}\n#{amz_headers}\n#{S3_BUCKET}#{object_name}"
   sig = CGI::escape(Base64.strict_encode64(OpenSSL::HMAC.digest('sha1', S3_SECRET, string_to_sign)))
 
-  CGI::escape("#{S3_URL}#{S3_BUCKET}#{object_name}?AWSAccessKeyId=#{S3_KEY}&Expires=#{expires}&Signature=#{sig}")
+  json :url => CGI::escape("#{S3_URL}#{S3_BUCKET}#{object_name}?AWSAccessKeyId=#{S3_KEY}&Expires=#{expires}&Signature=#{sig}")
 end
